36be56a78714a6512287c2846c5c0dcc32d6cd58
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2024-09-16 00:58:04 +0100

    Update utils.py

--- responsibly/we/utils.py
+++ responsibly/we/utils.py
@@ -211,7 +211,7 @@ def most_similar(model, positive=None, negative=None,
     
     ### Editted
     limited = (model.wv.get_vector() if restrict_vocab is None
-               else model.wv.get_vector([:restrict_vocab]))
+               else model.wv.get_vector[:restrict_vocab])
     dists = limited @ mean
 
     if topn is None:
