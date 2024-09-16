00ee146fbb4584f0f9eb36629a6efd9627a9f560
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2024-09-16 01:58:25 +0100

    Update utils.py

--- responsibly/we/utils.py
+++ responsibly/we/utils.py
@@ -83,7 +83,7 @@ def cosine_similarities_by_words(model, word, words):
 
 def update_word_vector(model, word, new_vector):
     model.vectors[model.key_to_index[word]] = new_vector
-    if model.get_vector(word) is not None:
+    if model.get_vector is not None:
         # model.vectors_norm[model.key_to_index[word]] = normalize(new_vector)
         model.get_vector(word, norm=True) == normalize(new_vector)
       
